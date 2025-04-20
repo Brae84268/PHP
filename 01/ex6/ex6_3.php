fa4a0d8d20cdb2823b92f63758bdae2d8d04c60a
Brae84268 <kjh84268@gmail.com>
2025-04-20 16:39:16 +0900

    6강

---
+++ 01/ex6/ex6_3.php
@@ -0,0 +1,15 @@
+<?php
+$score = 90;
+
+if ($score >= 90) {
+    echo "A학점 입니다.";
+} else if ($score >= 80) {
+    echo "B학점 입니다.";
+} else if ($score >= 70) {
+    echo "C학점 입니다.";
+} else if ($score >= 60) {
+    echo "D학점 입니다.";
+} else {
+    echo "B학점 입니다.";
+}
+?>
---
+++ 01/ex6/ex6_5.php
@@ -0,0 +1,15 @@
+<?php
+$num = 1;
+
+if ($num == 1) {
+    echo "east";
+} else if ($num == 2) {
+    echo "west";
+} else if ($num == 3) {
+    echo "south";
+} else if ($num == 4) {
+    echo "north";
+} else {
+    echo "wrong";
+}
+?>
