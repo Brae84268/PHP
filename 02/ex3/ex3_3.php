749a4af4aeaf563c5f7f725dbaac46a63a2381bd
Brae84268 <kjh84268@gmail.com>
2025-04-22 22:59:33 +0900

    3강

---
+++ 02/ex3/ex3_3.php
@@ -0,0 +1,7 @@
+<?php
+    $array = array("one" => "apple", "two" => "orange", "three" => "banana");
+
+    echo $array["one"]."<br>";
+    echo $array["two"]."<br>";
+    echo $array["three"]."<br>";
+?>
